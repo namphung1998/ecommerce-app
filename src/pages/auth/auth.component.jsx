22fc1c6ea3bc12715e95df3a58d0aeec70ed16b7
Nam Phung <nphung@macalester.edu>
2019-08-06 21:42:37 -0500

    added styling for buttons

---
+++ src/pages/auth/auth.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './auth.styles.scss';
+
+function Auth() {
+  return (
+    <div className='sign-in-and-sign-up'>
+      <SignIn />
+    </div>
+  );
+}
+
+export default Auth;
